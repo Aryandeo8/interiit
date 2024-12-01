7481da4dad9b4c39bbe62c1ef2be67d0fa345443
Aryandeo8 <aryandeo949@gmail.com>
2024-12-01 17:43:26 +0530

    final commit 2

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/option",
+      callbackURL: "https://interiit-cvd2.onrender.com/auth/google/option",
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
     async (accessToken, refreshToken, profile, cb) => {
